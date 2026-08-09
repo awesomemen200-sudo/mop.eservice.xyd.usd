c098eb45b4b05d388ce71996bf88579738c9a59e
awesomemen200-sudo <awesomemen200@gmail.com>
2026-08-10 00:30:17 +0300

    Delete mop-simple directory

--- mop-simple/files.js
+++
@@ -1,6 +0,0 @@
-// List every PDF you put in the "pdfs" folder here, exactly as it's named.
-// Keep the quotes and commas. Add or remove lines as needed.
-
-const FILES = [
-  "sample-report.pdf",
-];
